f95bf440891bdd73c4a21f255c03bae3d5f4da22
Aures AZ <assogbazeheaures@gmail.com>
2024-02-11 20:47:32 +0100

    correction-couleur&arrangements

---
+++ script.js
@@ -0,0 +1,14 @@
+// Récupérer l'élément span
+var span = document.getElementById('animation1');
+
+// Fonction pour faire apparaître/disparaître le texte
+function toggleText() {
+    // Si le texte est visible, le cacher ; sinon, le rendre visible
+    if (span.style.visibility === 'visible') {
+        span.style.visibility = 'hidden';
+    } else {
+        span.style.visibility = 'visible';
+    }
+}
+// Appeler la fonction toggleText() toutes les 2 secondes
+setInterval(toggleText, 2000);
